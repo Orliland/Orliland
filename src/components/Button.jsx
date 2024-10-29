e00874f61229a1f54ac403093ce5d4b3a1b52458
Orlando Verdin <git@orli.land>
2024-10-29 12:34:38 -0700

    fix button line-height

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 export default function Button({ className = "default" }) {
   return (
     <button
-      className={`rounded-full px-7 py-2 text-sm font-bold text-light-cream md:px-[45px] md:py-[14px] md:text-base ${className === "default" ? "bg-black hover:bg-purple" : ""} ${className === "secondary" ? "bg-red hover:bg-orange" : ""}`}
+      className={`rounded-full px-7 py-2 text-sm font-bold leading-7 text-light-cream md:px-[45px] md:py-[14px] md:text-base ${className === "default" ? "bg-black hover:bg-purple" : ""} ${className === "secondary" ? "bg-red hover:bg-orange" : ""}`}
     >
       Free Consultation
     </button>
