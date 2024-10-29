05730dfed32cff01355312a3ac4743d23a69c282
Orlando Verdin <git@orli.land>
2024-10-28 19:04:16 -0700

    update buttons styles for tablet and desktop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ className = "default" }) => {
   return (
     // TODO: add styles for tablet and desktop
     <button
-      className={`rounded-full px-7 py-2 text-sm font-bold text-light-cream ${className === "default" ? "bg-black hover:bg-purple" : ""} ${className === "secondary" ? "bg-red hover:bg-orange" : ""}`}
+      className={`rounded-full px-7 py-2 text-sm font-bold text-light-cream md:px-[45px] md:py-[14px] md:text-base ${className === "default" ? "bg-black hover:bg-purple" : ""} ${className === "secondary" ? "bg-red hover:bg-orange" : ""}`}
     >
       Free Consultation
     </button>
