cca8964ec8451541fd112cfa1025fda915b5d9b1
Orlando Verdin <git@orli.land>
2024-10-29 18:44:47 -0700

    add gap between bento and title

--- src/layout/Features.jsx
+++ src/layout/Features.jsx
@@ -2,7 +2,7 @@ import Bento from "./Bento";
 
 const Features = () => {
   return (
-    <section className="mx-auto mt-4 flex flex-col gap-8 px-4 md:mt-[30px] md:px-[39px] lg:mt-8 lg:w-9/12 lg:px-0">
+    <section className="mx-auto mt-4 flex flex-col gap-8 px-4 md:mt-[30px] md:gap-16 md:px-[39px] lg:mt-8 lg:w-9/12 lg:gap-20 lg:px-0">
       <div className="mx-auto flex max-w-[752px] flex-col gap-4 text-center md:gap-[27px] lg:gap-[23px]">
         <h1 className="text-[36px] font-bold md:text-[44px] lg:text-[56px]">
           Design solutions made easy
