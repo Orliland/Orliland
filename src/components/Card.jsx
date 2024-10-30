1cff79a2227f977186fee3e60f3debf88f1c5ee6
Orlando Verdin <git@orli.land>
2024-10-29 18:38:43 -0700

    add desktop and tablet styles for bento

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,9 @@ const Card = ({ className, icon, children }) => {
   return (
     <div className={`flex flex-col rounded-lg p-6 ${className}`}>
       <img src={icon} alt="" className="ml-auto" />
-      <span className="text-[24px] font-bold text-white">{children}</span>
+      <span className="text-[24px] font-bold leading-[30px] text-white">
+        {children}
+      </span>
     </div>
   );
 };
--- src/layout/Bento.jsx
+++ src/layout/Bento.jsx
@@ -9,27 +9,36 @@ import UiUxIcon from "../assets/pattern-ui-ux.svg";
 
 const Bento = () => {
   return (
-    <div className="grid grid-cols-2 gap-6">
+    <div className="grid grid-cols-2 items-start gap-6 md:grid-cols-4 lg:grid-cols-6 lg:grid-rows-2">
       <Card
-        className="col-span-full gap-[94px] bg-purple"
+        className="col-span-full gap-[94px] bg-purple md:col-span-2 md:row-span-2"
         icon={GraphicDesignIcon}
       >
         Graphic Design
       </Card>
-      <Card className="col-span-1 gap-10 bg-orange" icon={UiUxIcon}>
+      <Card
+        className="col-span-1 gap-10 bg-orange md:row-span-1"
+        icon={UiUxIcon}
+      >
         UI/UX
       </Card>
-      <Card className="col-span-1 gap-10 bg-pink" icon={AppsIcon}>
+      <Card className="col-span-1 gap-10 bg-pink md:row-span-1" icon={AppsIcon}>
         Apps
       </Card>
-      <Card className="col-span-full gap-4 bg-red" icon={IllustrationsIcon}>
+      <Card
+        className="col-span-full gap-4 bg-red md:col-span-2 lg:order-4"
+        icon={IllustrationsIcon}
+      >
         Illustrations
       </Card>
-      <Card className="col-span-full gap-10 bg-green" icon={PhotographyIcon}>
+      <Card
+        className="col-span-full gap-10 bg-green md:col-span-2 lg:order-3"
+        icon={PhotographyIcon}
+      >
         Photography
       </Card>
       <Card
-        className="col-span-full gap-10 bg-magenta"
+        className="col-span-full gap-10 bg-magenta md:col-span-2 lg:order-5 lg:gap-4"
         icon={MotionGraphicsIcon}
       >
         Motion Graphics
