8f993a6cc8f8eae9dd94ef83d7c415423b86c774
Orlando Verdin <git@orli.land>
2024-12-03 14:21:21 -0700

    update contact me info

--- src/layout/ContactCard.jsx
+++ src/layout/ContactCard.jsx
@@ -5,16 +5,16 @@ const ContactCard = () => {
     <section className="mx-4 flex flex-col items-center gap-6 rounded-[10px] bg-black px-6 py-12 md:mx-10 md:gap-[26px] md:px-[75px] md:pb-16 md:pt-14 lg:mx-auto lg:w-9/12 lg:flex-row lg:justify-between lg:p-20">
       <div className="flex flex-col gap-6 text-center text-light-cream md:gap-[26px] lg:max-w-[540px] lg:text-left">
         <h2 className="text-[26px] font-bold md:text-[32px] lg:text-[40px]">
-          Book a call with me
+          Get in Touch
         </h2>
         <p className="font-medium md:text-lg">
-          I’d love to have a chat to see how I can help you. The best first step
-          is for us to discuss your project during a free consultation. Then we
-          can move forward from there.
+          I’m always excited to collaborate on impactful projects and bring
+          ideas to life through elegant code and design. If you’re looking for a
+          passionate frontend developer to join your team, let’s connect!
         </p>
       </div>
       <Button className="secondary" action="mailto:hello@orli.land">
-        Contact me
+        Email Me
       </Button>
     </section>
   );
