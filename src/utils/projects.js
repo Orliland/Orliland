1fc7a5adae3cb36f3dd62110205abb63ffa6dd3e
Orlando Verdin <git@orli.land>
2024-12-17 00:16:58 -0700

    add product list with cart project

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -1,12 +1,12 @@
 const projects = [
   {
     id: 1,
-    thumbnail: "/images/image-slide-1.jpg",
-    title: "project title",
+    thumbnail: "/images/cart.webp",
+    title: "Product list with cart",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, minus architecto nam adipisci iste fugiat tempore laborum distinctio odit sequi ullam soluta earum, illo expedita quidem mollitia voluptate quia natus.",
-    repository: "https://github.com/Orliland",
-    live: "https://orli.land",
+      "An interactive project where users can add and remove products, adjust quantities, confirm orders, and reset their selections. The data is persisted using localStorage, built with React and Tailwind CSS, the design is fully responsive and optimized for different devices.",
+    repository: "https://github.com/Orliland/product_list_with_cart",
+    live: "https://product-list-with-cart-psi-seven.vercel.app/",
   },
 ];
 
