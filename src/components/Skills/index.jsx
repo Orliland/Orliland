4e36142d4d966f74ed88118749e97701839d00e3
Orlando Verdin <git@orli.land>
2026-07-14 13:49:19 -0700

    Add skills component and individual skill

---
+++ src/components/Skills/index.jsx
@@ -0,0 +1,15 @@
+import "./skills.css";
+
+function Skills({ skills }) {
+  return (
+    <div className="skills">
+      {skills.map((skill, index) => (
+        <span className="skill body-small" key={index}>
+          {skill}
+        </span>
+      ))}
+    </div>
+  );
+}
+
+export default Skills;
